a68feec517b3deee6067033b83bad3a79675d744
Julien Danjou <julien@danjou.info>
2017-12-31 18:13:55 +0100

    Test boursorama
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- greenpoint/tests/test_instrument.py
+++ greenpoint/tests/test_instrument.py
@@ -25,3 +25,20 @@ def test_quotes_from_lesechos():
                             high=17.69,
                             low=16.25,
                             volume=252461) in quotes
+
+
+def test_quotes_from_boursorama():
+    inst = instrument.Instrument(
+        isin="FR0011665280",
+        type=instrument.InstrumentType.STOCK,
+        name="Figeac Aero",
+        symbol="FGA",
+        exchange=instrument.get_exchange_by_mic("XPAR"),
+        pea=None, pea_pme=None, ttf=None)
+    quotes = inst.get_quotes_from_boursorama()
+    assert instrument.Quote(date=datetime.date(2017, 12, 20),
+                            open=16.73,
+                            close=17.69,
+                            high=17.69,
+                            low=16.25,
+                            volume=179707) in quotes
